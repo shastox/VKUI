2201f16a59c71ddb3c96af07170a8af9800984e8
Evgeniya <e.polozova@corp.vk.com>
2021-02-08 19:51:29 +0300

    FormField: do initial before&after implementation

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -1,20 +1,38 @@
-import React, { AllHTMLAttributes, ElementType, useState } from 'react';
+import React, { AllHTMLAttributes, ElementType, ReactNode, useState } from 'react';
 import { getClassName } from '../../helpers/getClassName';
 import { classNames } from '../../lib/classNames';
 import { usePlatform } from '../../hooks/usePlatform';
 import { HasRootRef } from '../../types';
+import { hasReactNode } from '@vkontakte/vkjs';
+import { withAdaptivity, AdaptivityProps } from '../../hoc/withAdaptivity';
+
+export interface FormFieldOwnProps {
+  /**
+   * Иконка или кнопка 24.
+   */
+  before?: ReactNode;
+  /**
+   * Иконка или кнопка 24.
+   */
+  after?: ReactNode;
+}
 
 export interface FormFieldProps extends
   AllHTMLAttributes<HTMLElement>,
-  HasRootRef<HTMLElement> {
+  HasRootRef<HTMLElement>,
+  FormFieldOwnProps,
+  AdaptivityProps {
   Component?: ElementType;
 }
 
-const FormField: React.FunctionComponent<FormFieldProps> = ({
+const FormField: React.FunctionComponent<FormFieldProps> = withAdaptivity(({
   Component,
   className,
   children,
   getRootRef,
+  before,
+  after,
+  sizeY,
   ...restProps
 }: FormFieldProps) => {
   const platform = usePlatform();
@@ -36,15 +54,27 @@ const FormField: React.FunctionComponent<FormFieldProps> = ({
       ref={getRootRef}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={classNames(getClassName('FormField', platform), className)}
+      className={classNames(getClassName('FormField', platform), `FormField--sizeY-${sizeY}`, className)}
     >
+      {hasReactNode(before) && (
+        <div className="FormField__before">
+          {before}
+        </div>
+      )}
       {children}
+      {hasReactNode(after) && (
+        <div className="FormField__after">
+          {after}
+        </div>
+      )}
       <div className={classNames('FormField__border', {
         'FormField__border--hover': hover,
       })} />
     </Component>
   );
-};
+}, {
+  sizeY: true,
+});
 
 FormField.defaultProps = {
   Component: 'div',
