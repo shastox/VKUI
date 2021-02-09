a123cbd1e703629249f3aceeea81d0b988ace711
Vladimir Klepov <v.klepov@gmail.com>
2021-02-09 18:01:24 +0300

    WIP: Improve createScopedElement perf

--- src/createScopedElement.tsx
+++ src/createScopedElement.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-const hasTransformable = /\b[A-Z]/g;
-const createScopedElement = (el: any, ...args: any[]) => {
-  const props = args[0];
-  if (typeof el === 'string' && props && props.className && props.className.match(hasTransformable)) {
-    props.className = (props.className as string)
-      .split(/\s+/g)
-      .map((cn) => cn.match(hasTransformable) ? `vkui__vkui__${cn}` : cn)
-      .join(' ');
+const hasTransformable = /\b(?=[A-Z])/g;
+const cache: any = {};
+function createScopedElement(el: any, props: any) {
+  let args = arguments;
+
+  if (typeof el !== 'string' || !props || !props.className) {
+    return React.createElement.apply(undefined, args);
   }
-  return React.createElement(el, ...args);
+
+  let argsLength = args.length;
+  let createElementArgArray = new Array(argsLength);
+  createElementArgArray[0] = args[0];
+
+  cache[props.className] = cache[props.className] || props.className.replace(hasTransformable, 'vkui__vkui__');
+  props.className = cache[props.className];
+  createElementArgArray[1] = props;
+
+  for (let i = 2; i < argsLength; i++) {
+    createElementArgArray[i] = args[i];
+  }
+
+  return React.createElement.apply(null, createElementArgArray);
 };
 createScopedElement.Fragment = React.Fragment;
 
