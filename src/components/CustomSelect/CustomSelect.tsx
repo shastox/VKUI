29b2a9decca01f71d94ffe0cbaab03cedee7fb2b
Evgeniya <e.polozova@corp.vk.com>
2021-02-09 18:55:14 +0300

    CustomSelect: update to include FormFieldProps

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -14,6 +14,7 @@ import { withAdaptivity } from '../../hoc/withAdaptivity';
 import { withPlatform } from '../../hoc/withPlatform';
 import CustomSelectOption, { CustomSelectOptionProps } from '../CustomSelectOption/CustomSelectOption';
 import { getClassName } from '../../helpers/getClassName';
+import { FormFieldProps } from '../FormField/FormField';
 
 type SelectValue = SelectHTMLAttributes<HTMLSelectElement>['value'];
 
@@ -31,7 +32,7 @@ interface CustomSelectState {
   selectedOptionIndex: number;
 }
 
-export interface CustomSelectProps extends NativeSelectProps {
+export interface CustomSelectProps extends NativeSelectProps, FormFieldProps {
   options: Array<{
     value: SelectValue;
     label: string;
